a5c75619b8d57ed41e2644876e92b178e49d4101
Sebastian Pereira <sebastian.pereira@pragma.com.co>
2023-10-17 06:56:42 -0500

    Kubernetes architecture impl

--- payment_service/src/main/java/com/pragma/payment_service/exception/OrderCustomException.java
+++ payment_service/src/main/java/com/pragma/payment_service/exception/OrderCustomException.java
@@ -2,8 +2,10 @@ package com.pragma.payment_service.exception;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 public class OrderCustomException extends RuntimeException{
 
